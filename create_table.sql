8db9ba0f9943268d05709060f1d90177ab4df031
Yusun <solarpy@hanyang.ac.kr>
2022-04-24 20:04:19 +0900

    Create mysql table

---
+++ create_table.sql
@@ -0,0 +1,31 @@
+CREATE TABLE weather (
+    id int auto_increment primary key,
+    date DATETIME,
+    main VARCHAR(30),
+    temp float,
+    feels_like float,
+    wind_speed float
+);
+
+CREATE TABLE distance (
+    station_id VARCHAR(50) primary key,
+    station_lat double,
+    station_lon double,
+    station_name VARCHAR(50),
+    mt1 int,
+    cs2 int,
+    sc4 int,
+    ac5 int,
+    sw8 int,
+    ct1 int
+);
+
+CREATE TABLE bike (
+    id int auto_increment primary key,
+    rack_tot_cnt int,
+    parking_bike_tot_cnt int,
+    shared int,
+    station_id VARCHAR(50),
+    date DATETIME,
+    foreign key (station_id) references distance(station_id) on update cascade on delete restrict
+);
